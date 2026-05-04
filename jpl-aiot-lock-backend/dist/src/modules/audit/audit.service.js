71dcd40c1156697145ffdb500c81be96cd192225
RIDSdiseno <diseno@rids.cl>
2026-05-04 15:12:19 -0400

    gitignore

--- jpl-aiot-lock-backend/dist/src/modules/audit/audit.service.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createAuditLog = createAuditLog;
-const prisma_1 = require("../../config/prisma");
-function createAuditLog(data) {
-    return prisma_1.prisma.auditLog.create({ data });
-}
